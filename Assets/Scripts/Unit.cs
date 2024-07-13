ec3a76e01c31126460b45b8d792d966b8d7a9748
QuantumTrolling <st097173@student.spbu.ru>
2024-07-13 18:43:12 +0300

    rework

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public bool isEnemy;
+    public int health;
+    public int speed;
+
+}
