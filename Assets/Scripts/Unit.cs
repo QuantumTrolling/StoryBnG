c287e7f1307ca0de6f1c3bf6afb50d720468f827
QuantumTrolling <st097173@student.spbu.ru>
2024-07-16 22:47:25 +0300

    Bob&Scul

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    public bool isEnemy;
-    public int health;
-    public int speed;
+    [SerializeField] public bool isEnemy;
+    [SerializeField] public float Max_health;
+    [SerializeField] public int speed;
+
+    private float current_health;
+
+    public void TakeDamage(float damage){
+        current_health=-damage;
+        Debug.Log(gameObject.name + " current health is " + current_health);
+        if (current_health <= 0){
+            Death();
+        }
+    }
+
+    public void Death(){
+        Destroy(gameObject);
+    }
 
 }
