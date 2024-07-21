1fc3322b7499ec8365e705eaf6da587326fc8c28
QuantumTrolling <st097173@student.spbu.ru>
2024-07-21 18:36:02 +0300

    tips

---
+++ Assets/Scripts/ShowTips.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShowTips : MonoBehaviour
+{
+    [SerializeField] GameObject tips;
+
+    private void Start() {
+        tips.SetActive(false);
+    }
+
+    private void OnDisable() {
+        tips.SetActive(false);
+    }
+
+    private void OnMouseOver() {
+        tips.SetActive(true);
+    }
+
+    private void OnMouseExit() {
+        tips.SetActive(false);
+    }
+
+}
